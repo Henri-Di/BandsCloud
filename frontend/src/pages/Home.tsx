1cd740efcdbd4e8d7f0131d93068afb8f7401d5f
Matheus Diamantino <matheus.diamantino20@gmail.com>
2025-07-10 20:29:17 -0300

    feat: aAtualizações nos componentes do Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -218,7 +218,7 @@ export default function Home() {
               className="text-purple-400 animate-pulse"
               aria-hidden="true"
             />
-            Recursos do BandsCloud
+            Recursos
           </h2>
 
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 text-left">
